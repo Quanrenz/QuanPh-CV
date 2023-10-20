0b782f9f5f359f593adf72dc31ca72e14615a7b2
Quanrenz <quanpham2082002@gmail.com>
2023-10-20 21:42:02 +0700

    fix_img

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ServicesComponent } from './Components/services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HeaderComponent,
+    HomeComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,

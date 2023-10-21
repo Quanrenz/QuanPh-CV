ae4c8778d80916867b9112ffb2505ca9e2dbaa8a
Quanrenz <quanpham2082002@gmail.com>
2023-10-21 23:08:01 +0700

    update_21_10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './Components/contact/contact.component';
+import { AboutComponent } from './Components/about/about.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ServicesComponent } from './Components/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Contact', component: ContactComponent
+  },
+  {
+    path: 'About', component: AboutComponent
+  },
+  {
+    path: 'Home', component: HomeComponent
+  },
+  {
+    path: 'Services', component: ServicesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
